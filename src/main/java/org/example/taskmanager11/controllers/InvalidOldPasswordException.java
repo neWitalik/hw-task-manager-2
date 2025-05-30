834917c9ec78bffa3f47957cbe3ab8c0824a7e0d
neVitalik <dan.jah067@gmail.com>
2025-05-30 22:31:58 +0200

    плюс дз

--- src/main/java/org/example/taskmanager11/controllers/InvalidOldPasswordException.java
+++
@@ -1,7 +0,0 @@
-package org.example.taskmanager11.controllers;
-
-public class InvalidOldPasswordException extends RuntimeException {
-    public InvalidOldPasswordException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/example/taskmanager11/controllers/PasswordsDoNotMatchException.java
+++
@@ -1,7 +0,0 @@
-package org.example.taskmanager11.controllers;
-
-public class PasswordsDoNotMatchException extends RuntimeException {
-    public PasswordsDoNotMatchException(String message) {
-        super(message);
-    }
-}
